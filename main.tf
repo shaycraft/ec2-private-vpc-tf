0da547a95d72d3b097f245f90c14938da90ef34f
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2022-12-26 00:36:27 -0700

    feat: adds ec2 wordpress instance

--- main.tf
+++ main.tf
@@ -8,4 +8,45 @@ module "vpc" {
   tags                         = { "Name" = "terraform vpc" }
   create_database_subnet_group = false
   public_subnet_tags           = { "Name" = "public subnet terraform" }
+  enable_dns_hostnames         = true
+}
+
+resource "aws_instance" "wordpress" {
+  ami           = "ami-0397abf91212932d1"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "wordpress ec2 terraform"
+  }
+  subnet_id              = module.vpc.public_subnets[0]
+  vpc_security_group_ids = [aws_security_group.main.id]
+}
+
+resource "aws_security_group" "main" {
+  vpc_id = module.vpc.vpc_id
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
 }
