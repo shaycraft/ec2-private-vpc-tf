3287298ff84d7382e3acb7e61d63fa58a32db87f
Sam <phreakdawg@gmail.com>
2022-12-26 00:52:40 -0700

    Feature/ec2 (#1)
    * feat: adds ec2 wordpress instance
    
    * refactor: tfvars variables for config
    
    * fix: fixes tfvars name
    
    Co-authored-by: Sam Haycraft <samuel.g.haycraft@xcelenergy.com>

--- main.tf
+++ main.tf
@@ -2,10 +2,51 @@ data "aws_availability_zones" "azs" {}
 
 module "vpc" {
   source                       = "terraform-aws-modules/vpc/aws"
-  cidr                         = "10.0.0.0/16"
-  public_subnets               = ["10.0.1.0/24"]
+  cidr                         = var.vpc_cidr
+  public_subnets               = var.public_subnets
   azs                          = data.aws_availability_zones.azs.names
   tags                         = { "Name" = "terraform vpc" }
   create_database_subnet_group = false
   public_subnet_tags           = { "Name" = "public subnet terraform" }
+  enable_dns_hostnames         = true
+}
+
+resource "aws_instance" "wordpress" {
+  ami           = var.ami
+  instance_type = "t2.micro"
+  tags = {
+    Name = "wordpress ec2 terraform"
+  }
+  subnet_id              = module.vpc.public_subnets[0]
+  vpc_security_group_ids = [aws_security_group.main.id]
+}
+
+resource "aws_security_group" "main" {
+  vpc_id = module.vpc.vpc_id
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
 }
--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,15 @@ output "subnet_public_cidr" {
 output "subnet_public_ids" {
   value = module.vpc.public_subnets
 }
+
+output "wordpress_dns" {
+  value = aws_instance.wordpress.public_dns
+}
+
+output "wordpress_ip_public" {
+  value = aws_instance.wordpress.public_ip
+}
+
+output "wordpress_ip_private" {
+  value = aws_instance.wordpress.private_ip
+}
