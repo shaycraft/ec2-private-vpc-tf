1d8349e0ea8ff68fcfc4550d5e517357525a6691
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2022-12-31 21:25:53 -0700

    feat: creates private vpc

--- main.tf
+++ main.tf
@@ -4,11 +4,34 @@ module "vpc" {
   source                       = "terraform-aws-modules/vpc/aws"
   cidr                         = var.vpc_cidr
   public_subnets               = var.public_subnets
+  private_subnets              = var.private_subnets
   azs                          = data.aws_availability_zones.azs.names
   tags                         = { "Name" = "terraform vpc" }
   create_database_subnet_group = false
   public_subnet_tags           = { "Name" = "public subnet terraform" }
+  private_subnet_tags          = { "Name" = "private subnet terraform" }
   enable_dns_hostnames         = true
+  enable_nat_gateway           = true
+  default_network_acl_egress = [
+    {
+      protocol   = -1
+      rule_no    = 100
+      action     = "allow"
+      cidr_block = "0.0.0.0/0"
+      from_port  = 0
+      to_port    = 0
+    }
+  ]
+  default_network_acl_ingress = [
+    {
+      protocol   = -1
+      rule_no    = 100
+      action     = "allow"
+      cidr_block = "0.0.0.0/0"
+      from_port  = 0
+      to_port    = 0
+  }]
+
 }
 
 resource "aws_instance" "wordpress" {
@@ -17,7 +40,8 @@ resource "aws_instance" "wordpress" {
   tags = {
     Name = "wordpress ec2 terraform"
   }
-  subnet_id              = module.vpc.public_subnets[0]
+  #  subnet_id              = module.vpc.public_subnets[0]
+  subnet_id              = module.vpc.private_subnets[0]
   vpc_security_group_ids = [aws_security_group.main.id]
 }
 
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,3 +1,4 @@
-ami            = "ami-0397abf91212932d1"
-vpc_cidr       = "10.0.0.0/16"
-public_subnets = ["10.0.1.0/24"]
+ami             = "ami-0397abf91212932d1"
+vpc_cidr        = "10.0.0.0/16"
+public_subnets  = ["10.0.1.0/24"]
+private_subnets = ["10.0.10.0/24"]
