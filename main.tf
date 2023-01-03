e99ef0aca916d533341e8688c9f87ee07232d3db
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2023-01-02 22:33:58 -0700

    refactor:  removes security group module (pos that does not work)

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ resource "aws_instance" "nginx" {
     Name = "nginx ubuntu terraform"
   }
   subnet_id              = module.vpc.public_subnets[0]
-  vpc_security_group_ids = [module.security_group.security_group_id]
+  vpc_security_group_ids = [aws_security_group.main.id]
 
   connection {
     type        = "ssh"
@@ -72,19 +72,58 @@ resource "aws_instance" "wordpress" {
     Name = "wordpress ec2 terraform"
   }
   subnet_id              = module.vpc.private_subnets[0]
-  vpc_security_group_ids = [module.security_group.security_group_id]
+  vpc_security_group_ids = [aws_security_group.main.id]
+  security_groups        = []
 }
 
-module "security_group" {
-  source              = "terraform-aws-modules/security-group/aws"
-  name                = "main default security group for ec2 instances"
-  vpc_id              = module.vpc.vpc_id
-  egress_rules        = ["all-all"]
-  ingress_rules       = ["all-all"]
-  egress_cidr_blocks  = ["0.0.0.0/0"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  tags = {
-    "Name" = "terraform security-group main"
-  }
+resource "aws_security_group" "main" {
+  vpc_id = module.vpc.vpc_id
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = "default egress all rule"
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = -1
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = "SSH"
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = "HTTP"
+      from_port        = 80
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 80
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = "HTTPS/TLS"
+      from_port        = 443
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 443
+    }
+  ]
 }
