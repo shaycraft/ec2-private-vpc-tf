bafe247a061e8fa674462d19352986cd5e8982c6
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2023-01-03 21:45:49 -0700

    refactor: extracts security group to custom module

---
+++ modules/security_groups/main.tf
@@ -0,0 +1,51 @@
+resource "aws_security_group" "main" {
+  vpc_id = var.vpc
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = "default egress all rule"
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = -1
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = "SSH"
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = "HTTP"
+      from_port        = 80
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 80
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = "HTTPS/TLS"
+      from_port        = 443
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 443
+    }
+  ]
+}
---
+++ modules/security_groups/variables.tf
@@ -0,0 +1,8 @@
+variable "allow_ports" {
+  type = list(number)
+}
+
+variable "vpc" {
+  type        = string
+  description = "vpc id"
+}
