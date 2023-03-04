7c6b501a17dfe931a31107a077f1ebeac6dd42ac
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2023-03-03 22:43:28 -0700

    feat(issue-9): Implements user_data to provision nginx

--- main.tf
+++ main.tf
@@ -59,13 +59,30 @@ resource "aws_instance" "nginx" {
     timeout     = "4m"
   }
 
-  provisioner "remote-exec" {
-    inline = [
-      # see https://serverfault.com/questions/478461/how-to-install-packages-with-apt-without-user-interaction
-      "sudo apt-get update && sudo apt-get -y install nginx",
-      "sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj '/C=US'"
-    ]
+  provisioner "file" {
+    content     = templatefile("${path.module}/config/nginx.config.tftpl", { proxy_pass = aws_instance.wordpress.private_ip })
+    destination = "/tmp/terraform-nginx.config"
   }
+
+  provisioner "file" {
+    source      = "${path.module}/config/self-signed.config"
+    destination = "/tmp/terraform-self-signed.config"
+  }
+
+  user_data = <<EOF
+#!/bin/bash
+echo "Installing nginx..."
+sudo apt-get update && sudo apt-get -y install nginx
+echo "Nginx install complete..."
+echo "Generating self-signed cert..."
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=US"
+echo "Attempting to copy nginx config now..."
+sudo cp -f /tmp/terraform-nginx.config /etc/nginx/sites-available/default
+echo "Attempting to copy cert config now..."
+sudo cp -f /tmp/terraform-self-signed.config /etc/nginx/snippets/self-signed.conf
+echo "Restarting nginx..."
+sudo systemctl restart nginx
+EOF
 }
 
 resource "aws_instance" "wordpress" {
